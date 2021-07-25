62f23da73294af5d4c2f79e22ce1ba419c56027e
kyj <fioonmp1001@gmail.com>
2021-07-25 22:37:24 +0900

    Repository 인터페이스 추가, Repository 테스트 코드 추가

---
+++ src/test/java/com/springboot/book/web/dto/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.springboot.book.web.dto.posts;
+
+import com.springboot.book.domain.posts.PostRepository;
+import com.springboot.book.domain.posts.Posts;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @After
+    public void cleanup(){
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postRepository.save(Posts.builder()
+                                    .title(title)
+                                    .content(content)
+                                    .author("jojoldu@gmail.com")
+                                    .build());
+
+        //when
+        List<Posts> postsList = postRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
